c66e6e40fcd5e5dc75402f314e539b1d14105f55
Keila Aranda <keila.arandah@gmail.com>
2024-09-26 22:53:31 -0600

    Update level.blade.php

--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -4,18 +4,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Usuarios de{{ $level->name }}</title>
+    <title>Usuarios de {{ $level->name }}</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
         crossorigin="anonymous">
+
+    <style>
+        body {
+            color: black; 
+        }
+        .badge {
+            color: #003366; 
+        }
+        .badge.bg-light {
+            background-color: #f8f9fa; 
+        }
+    </style>
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col-12 my-3 pt-3 shadow">
                
-                <h3>Contenido de usuarios nivel{{ $level->name }} </h3>
+                <h3>Contenido de usuarios nivel {{ $level->name }} </h3>
                 <hr>
                 <div class="row">
                     @foreach ($posts as $post)
@@ -28,7 +41,7 @@
                                     <div class="col-md-8">
                                         <div class="card-body">
                                           <h5 class="card-title">{{ $post->name }}</h5>
-                                          <h6 class="card-subtitle text-muted">
+                                          <h6 class="card-subtitle">
                                             {{ $post->category->name }} |
                                             {{ $post->comments_count }}
                                             {{ str()->plural('comentario', $post->comments_count) }}
@@ -36,7 +49,7 @@
                                           <p class="card-text small">
                                             @foreach ($post->tags as $tag )
                                             <span class="badge bg-light">
-                                                #{{ $tag->name}}
+                                                #{{ $tag->name }}
                                             </span>
                                             @endforeach
                                           </p>
@@ -48,7 +61,7 @@
                     @endforeach
                 </div>
 
-                <h3>Contenido de video de usuarios nivel{{ $level->name }} </h3>
+                <h3>Contenido de video de usuarios nivel {{ $level->name }} </h3>
                 <hr>
 
                 <div class="row">
@@ -62,7 +75,7 @@
                                     <div class="col-md-8">
                                         <div class="card-body">
                                            <h5 class="card-title">{{ $video->name }}</h5>
-                                           <h6 class="card-subtitle text-muted">
+                                           <h6 class="card-subtitle">
                                             {{ $video->category->name }} |
                                             {{ $video->comments_count }}
                                             {{ str()->plural('comentario', $video->comments_count) }}
@@ -70,7 +83,7 @@
                                           <p class="card-text small">
                                             @foreach ($video->tags as $tag )
                                             <span class="badge bg-light">
-                                                #{{ $tag->name}}
+                                                #{{ $tag->name }}
                                             </span>
                                             @endforeach
                                          </p>
